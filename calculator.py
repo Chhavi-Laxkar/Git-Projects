881fff02ea848d9539f10df6077734892e517171
Chhavi-Laxkar <chhavi.l4204@gmail.com>
2026-07-20 13:01:19 +0530

    Added Square root

--- calculator.py
+++ calculator.py
@@ -9,6 +9,8 @@ def division(num1 , num2):
     return num1 / num2
 def average(num1 , num2):
     return (num1 + num2) / 2
+def square_root(num1):
+    return num1 ** 0.5
 
 # Step 2 - User input :
 print("Please Select One Operations :\n"
@@ -16,10 +18,11 @@ print("Please Select One Operations :\n"
        "2.Subtraction\n"
        "3.Multiplication\n"
        "4.Division\n"
-       "5.Average\n")
+       "5.Average\n"
+         "6.Square Root\n")
 
 
-select = int(input("Select operations from 1,2,3,4,5 :"))
+select = int(input("Select operations from 1,2,3,4,5,6 :"))
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
@@ -35,5 +38,7 @@ elif select == 4 :
     print("Division =", division(num1 , num2))
 elif select == 5 :
     print("Average =", average(num1 , num2))    
-else :
+elif select == 6 :
+    print("Sqaure Root = ",square_root(num1))
+else:
     print("Invalid input")
